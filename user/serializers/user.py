cf29f3b3564d0acc1b9865a5a8960bba8c9a99f2
rockstar-programmer <rockstarrprogrammerr@gmail.com>
2021-08-23 01:01:27 +0700

    add password validator

--- user/serializers/user.py
+++ user/serializers/user.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 
 User = get_user_model()
 
@@ -15,6 +16,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password']
         extra_kwargs = {
             'password': {
-                'write_only': True
+                'write_only': True,
+                'validators': [validate_password]
             }
         }
