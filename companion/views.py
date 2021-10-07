111c53bf740f514a7c40abdba752d3607a79cbeb
rockstar-programmer <rockstarrprogrammerr@gmail.com>
2021-10-07 21:09:51 +0700

    list all APIs available in root API

--- companion/views.py
+++ companion/views.py
@@ -11,29 +11,34 @@ class RootAPIView(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request):
-        if request.user.is_authenticated:
+        if not request.user.is_authenticated:
             data = {
-                'my_info': reverse('user-my-info', request=request),
-                'my_event_invitations': reverse('user-my-event-invitation-list', request=request),
-                'user': request.build_absolute_uri(root_endpoints.USER),
+                'browsable_api_login': reverse('rest_framework:login', request=request)
+            }
+        elif request.user.is_staff:
+            data = {
                 'split_the_bill': request.build_absolute_uri(root_endpoints.SPLIT_THE_BILL),
+                'account': {
+                    'my_info': reverse('user-my-info', request=request),
+                    'my_event_invitations': reverse('user-my-event-invitation-list', request=request),
+                    'user': request.build_absolute_uri(root_endpoints.USER),
+                },
+                'authen': {
+                    'register': reverse('user-register', request=request),
+                    'login': reverse('token_obtain_pair', request=request),
+                    'refresh_token': reverse('token_refresh', request=request),
+                    'social_authen': {
+                        'google': reverse('social_account_google_authen', request=request),
+                        'facebook': reverse('social_account_facebook_authen', request=request),
+                    },
+                    'email_reset_password_link': reverse('user-email-reset-password-link', request=request),
+                    'reset_password': reverse('user-reset-password', request=request),
+                },
+                'test_error_logging': reverse('companion-test-error-logging', request=request),
             }
-            if request.user.is_staff:
-                data['test_error_logging'] = reverse('companion-test-error-logging', request=request)
         else:
-            data = {
-                'email_reset_password_link': reverse('user-email-reset-password-link', request=request),
-                'reset_password': reverse('user-reset-password', request=request),
-                'register': reverse('user-register', request=request),
-                'login': reverse('token_obtain_pair', request=request),
-                'refresh_token': reverse('token_refresh', request=request),
-                'social_authen': {
-                    'google': reverse('social_account_google_authen', request=request),
-                    'facebook': reverse('social_account_facebook_authen', request=request),
-                }
-            }
-            if settings.DEBUG:
-                data['browsable_api_login'] = reverse('rest_framework:login', request=request)
+            data = {}
+
         return Response(data)
 
 
